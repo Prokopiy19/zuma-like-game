f9779fa37ef7b7f311a7bd5a15c26b108c6108e9
Prokopiy Danilov <danilovprokopiy@gmail.com>
2023-09-18 00:29:06 +0900

    make balls faster

--- src/settings.h
+++ src/settings.h
@@ -5,8 +5,8 @@ constexpr float BALL_RADIUS = 1.0f;
 constexpr float SHOOTER_RADIUS = 3.0f;
 
 constexpr float FRICTION = 0.5f;
-constexpr float BALL_ACCEL = 1.5f;
-constexpr float BACK_ACCEL = 4.0f;
+constexpr float BALL_ACCEL = 3.0f;
+constexpr float BACK_ACCEL = 5.0f;
 
 constexpr static float SPAWN_SPEED = 20.0f;
 
