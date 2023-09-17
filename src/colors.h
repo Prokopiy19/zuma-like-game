c0aac5a6c41494459d48e39b85a0445d861aecd4
Prokopiy Danilov <danilovprokopiy@gmail.com>
2023-09-18 00:37:46 +0900

    add yellow color

--- src/colors.h
+++ src/colors.h
@@ -7,7 +7,8 @@
 #define X_COLOR_TEXTURES \
     X(COLOR_RED, "red") \
     X(COLOR_GREEN, "green") \
-    X(COLOR_BLUE, "blue")
+    X(COLOR_BLUE, "blue") \
+    X(COLOR_YELLOW, "yellow")
 
 enum Color {
     #define X(a, b) a,
